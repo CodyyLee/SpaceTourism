79c076c05f5f60befc1ac7a9f3fe0846b717a4f1
Cody Garrett <cody.l.garrett22@gmail.com>
2022-10-08 19:09:55 -0400

    tablet view of the landing page added in

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../../assets/shared/logo.svg';
 import burger from '../../assets/shared/icon-hamburger.svg';
 import './navbar.scss';
+import { NavLink } from 'react-router-dom';
 
 
 export default function Navbar() {
+
+    const [width, setWidth] = useState();
+
+    let activeStyle = {
+        textDecoration: 'underline',
+        textUnderlineOffset: `${37}px`,
+        textDecorationThickness: '3px'
+    }
+
+    const windowResize = () => {
+        const {innerWidth} = window;
+
+        return innerWidth;
+    }
+
+    useEffect(() => {
+        const resizeHandler = () => {
+            setWidth(windowResize());
+        }
+
+        window.addEventListener('resize', resizeHandler)
+
+        return () => {
+            window.removeEventListener('resize', resizeHandler);
+        }
+    }, []);
+
   return (
     <nav className='navbar'>
         <div className='logoContainer'>
@@ -12,9 +40,47 @@ export default function Navbar() {
         </div>
 
         <div className='navMenu'>
-            <button className='menuBtn'>
+            {width < 768 ? <button className='menuBtn'>
                 <img src={burger} alt='menu button' />
-            </button>
+            </button> : 
+            <ul className='navLinks'>
+                <li>
+                    <NavLink
+                    style={({ isActive }) =>
+                        isActive ? activeStyle : null
+                    }       
+                    className='link'
+                        to='/'
+                    >HOME</NavLink>
+                </li>
+                <li>
+                    <NavLink
+                    style={({ isActive }) =>
+                        isActive ? activeStyle : null
+                    }       
+                    className='link'
+                        to='/destination'
+                    >DESTINATION</NavLink>
+                </li>
+                <li>
+                    <NavLink
+                    style={({ isActive }) =>
+                        isActive ? activeStyle : null
+                    }       
+                    className='link'
+                        to='/crew'
+                    >CREW</NavLink>
+                </li>
+                <li>
+                    <NavLink
+                    style={({ isActive }) =>
+                        isActive ? activeStyle : null
+                    }       
+                    className='link'
+                        to='/technology'
+                    >TECHNOLOGY</NavLink>
+                </li>
+            </ul>}
         </div>
     </nav>
   )
